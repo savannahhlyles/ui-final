310d4eb16a699f8b0e6bb17cbcd798021b4d341c
Savannah Lyles <savannahhlyles513@gmail.com>
2025-04-18 17:50:44 -0400

    Added routes for pages

--- server.py
+++ server.py
@@ -11,9 +11,17 @@ app = Flask(__name__)
 def default():
    return render_template('home.html')   
 
-@app.route('/home')
-def home():
-   return render_template('home.html')   
+@app.route('/learn')
+def learn():
+   return render_template('learn.html')   
+
+@app.route('/quiz')
+def quiz():
+   return render_template('quiz.html')   
+
+@app.route('/procreate')
+def procreate():
+   return render_template('procreate.html')   
 
 
 
