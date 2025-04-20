731e6d357cd4555fdd7eb4b7c1faed6f9ed673a5
Savannah Lyles <savannahhlyles513@gmail.com>
2025-04-20 14:39:36 -0400

    More question data added

--- server.py
+++ server.py
@@ -8,11 +8,9 @@ questions = [
     {"id": 2, "text": "Fill in the blanks using the word bank to complete the statements.", "options": [], "answer": ""},
     {"id": 3, "text": "Which brush is best for light, initial construction lines before adding detail?", "options": ["Narinder Pencil", "6B Pencil", "HB Pencil", "Procreate Pencil"], "answer": "HB Pencil"},
     {"id": 4, "text": "Match each scenario to the most suitable brush for the task.", "options": [], "answer": ""},
-    {"id": 5, "text": "You’re sketching a rough pose and want the brush to respond well to tilt for expressive lines. Which brush do you choose?", "options": [], "answer": ""},
-    {"id": 6, "text": "Identify the brush used for this sketch.", "options": [], "answer": ""},
+    {"id": 5, "text": "You’re sketching a rough pose and want the brush to respond well to tilt for expressive lines. Which brush do you choose?", "options": ["HB Pencil", "Oil Pastel", "Narinder Pencil", "Peppermint"], "answer": "Peppermint"},
+    {"id": 6, "text": "Identify the brush used for this sketch.", "options": ["HB Pencil", "Procreate Pencil", "Oil Pastel", "6B Pencil"], "answer": "HB Pencil"},
 ]
-
-
 
 # ROUTES
 
@@ -39,8 +37,6 @@ def view_question(question_id):
 @app.route('/procreate')
 def procreate():
    return render_template('procreate.html')   
-
-
 
 if __name__ == '__main__':
    app.run(debug = True, port=5001)
